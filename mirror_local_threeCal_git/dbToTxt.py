2a13ddf93b433667538fa2e85facb75ab2cdde13
glen00 <ast90463git@gmail.com>
2021-09-29 01:00:48 +0800

    origin

--- mirror_local_threeCal_git/dbToTxt.py
+++ mirror_local_threeCal_git/dbToTxt.py
@@ -1,11 +1,13 @@
 import All_number
 import mysql.connector
 import sys
+import datetime
 
 import BBands
 import BIAS_v0
 
 isDebug = True
+logErrorTag = 'dbToTxt_Error'
 
 def setDate(dateNow=datetime.datetime.now().strftime('%Y%m%d')):
     return str(dateNow)
@@ -57,9 +59,23 @@ def getDBdata(num,dbName,dataTypeL):
     if isDebug: print(all_data[0])
     return all_data
 
+def procBBands(closeList,writeTXT):
+    H_v, M_v, L_v = BBands.main(closeList)
+    H_vStr = 'H_v=' + str(H_v) + '\n'
+    M_vStr = 'M_v=' + str(M_v) + '\n'
+    L_vStr = 'L_v=' + str(L_v) + '\n'
+    writeTXT.write(H_vStr)
+    writeTXT.write(M_vStr)
+    writeTXT.write(L_vStr)
+    del H_v,M_v,L_v,H_vStr,M_vStr,L_vStr
 
-def writeToTxt(dataType,allDataL):
-    fileName = 'testAldata_' + setDate() + '.txt'
+def procBIAS(closeList,writeTXT):
+    BIAS_36 = BIAS_v0.cal_36(closeList)
+    biasStr = 'bias_list=' + str(BIAS_36) + '\n'
+    writeTXT.write(biasStr)
+    del BIAS_36,biasStr
+
+def writeDBToTxt(dataType,allDataL,fileName):
     for i in range(len(dataType)):
         locals()[dataType[i] + '_List'] = list()
     #print(locals())
@@ -77,22 +93,39 @@ def writeToTxt(dataType,allDataL):
     for l in range(len(dataType)):
         wrStr = dataType[l] + '=' + str(locals()[dataType[l] + '_List']) + '\n'
         write_log.write(wrStr)
+        if dataType[l] == 'close_p':
+            procBBands(locals()[dataType[l] + '_List'], write_log)
+            procBIAS(locals()[dataType[l] + '_List'], write_log)
         del locals()[dataType[l] + '_List']
     write_log.close()
 	#del allDataL
 
+
+def writeTxt(dataStr,fileName):
+    writeStr = open(fileName, 'a', encoding='UTF-8')
+    writeStr.write(dataStr)
+    writeStr.close()
+
 def main():
+    fileName = 'testAldata_' + setDate() + '.txt'
     type = ['twse', 'tpex']
-    dataTypeL = ['high_p','low_p','close_p','date','open_p']
+    dataTypeL = ['high_p','low_p','close_p','date','open_p','type1','type2','type7','type8']
     for i in range(len(type)):
         stock_num_list, stock_num_list_type, stock_num_list_public_date = All_number.main(type[i])
         for k in range(len(stock_num_list)):
-            print(stock_num_list[k])
-            #get_price_allandDate(stock_num_list[k])
-            allDataList = getDBdata(stock_num_list[k],'stock1',dataTypeL)
-            writeToTxt(dataTypeL,allDataList)
-            break
-        break
+            try:
+                print(stock_num_list[k])
+                allDataList = getDBdata(stock_num_list[k],'stock1',dataTypeL)
+                writeTxt('-----------------' + str(stock_num_list[k]) + '-----------------' + '\n', fileName)
+                writeDBToTxt(dataTypeL,allDataList,fileName)
+            except Exception as e:
+                if isDebug: print(stock_num_list[k],' has some error! e=',e)
+                errorStr = logErrorTag + ': main: ' + str(stock_num_list[k]) + ' has some error! e=' + str(e)
+                writeTxt(errorStr,logErrorTag+'.txt')
+
+
+
+    clearAllAllocate()
 
 def clearAllAllocate():
     print(locals())
